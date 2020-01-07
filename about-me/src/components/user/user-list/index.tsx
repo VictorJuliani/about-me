9624ab986df45385308627a812e65d9def96914e
Victor Juliani <v_juliani@hotmail.com>
2020-01-07 17:11:59 -0300

    Implement user list layout

--- about-me/src/components/user/user-list/index.tsx
+++ about-me/src/components/user/user-list/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { List, Avatar, Card, Empty, Button } from 'antd';
 import { getUsers, UsersState } from '../../../store';
 import { User } from '../../../models';
 import './styles.scss';
-import { Link } from 'react-router-dom';
 
 interface UserListProps {
     users: User[];
@@ -12,14 +13,39 @@ interface UserListProps {
 class UserList extends React.Component<UserListProps> {
     render() {
         const users = this.props.users;
+        const placeholder = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png";
+    
+        const emptyUsers = (
+            <div style={{ textAlign: 'center' }}>
+                <Empty></Empty>
+                <Button type="primary" style={{ marginTop: 10 }}>
+                    <Link to="/users/new">Create User</Link>
+                </Button>
+            </div>
+        );
+
+        const userList = (
+            <List
+                itemLayout="horizontal"
+                dataSource={users}
+                renderItem={user => (
+                    <Link to={`/users/${user.id}`}>
+                        <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Avatar src={user.imgs && user.imgs.length ? user.imgs[0] : placeholder} />}
+                                    title={user.name}
+                                    description={user.job}
+                                    />
+                        </List.Item>
+                    </Link>
+                )}
+            />
+        );
+
         return (
-            <ul>
-                {
-                    users.map(u =>
-                        <li key={u.id}><Link to={`/users/${u.id}`}>{u.name}</Link></li>
-                    )
-                }
-            </ul>
+            <Card>
+                { !users.length ? emptyUsers : userList }
+            </Card>
         );
     }
 }
