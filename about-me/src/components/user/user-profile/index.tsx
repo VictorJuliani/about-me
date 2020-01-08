61084c8f0c438d5e3bec02cc208df1ac363ec692
Victor Juliani <v_juliani@hotmail.com>
2020-01-08 08:29:19 -0300

    Add LikeButton to user profile

--- about-me/src/components/user/user-profile/index.tsx
+++ about-me/src/components/user/user-profile/index.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, Redirect } from 'react-router';
 import { connect } from 'react-redux';
 import { Card } from 'antd';
-import { UsersState, getUser } from '../../../store';
+import { UsersState, getUser, likeUser } from '../../../store';
 import UserInfo from '../user-info';
 import UserAbout from '../user-about';
 import { User } from '../../../models';
 import './styles.scss';
+import LikeButton from '../../widgets/like';
 
 interface UserProfileProps {
     user: User | undefined;
+    like: (user: User) => void;
 }
 
-const UserProfile: React.FC<UserProfileProps> = ({ user }) => {
+const UserProfile: React.FC<UserProfileProps> = ({ user, like }) => {
+    const [liked, setLiked] = useState(false);
+    const handleLike = () => {
+        like(user as User);
+        setLiked(true);
+    };
+
     return !user
         ? <Redirect to="/" />
         : (
@@ -20,6 +28,7 @@ const UserProfile: React.FC<UserProfileProps> = ({ user }) => {
                 <Card className="user-card">
                     <UserInfo user={user} />
                     <UserAbout user={user} />
+                    <LikeButton onClick={handleLike} disabled={liked} />
                 </Card>
             </div>
         )
@@ -29,7 +38,11 @@ const mapStateToProps = (state: UsersState, ownProps: any) => ({
     user: getUser(state, ownProps.userId)
 });
 
-const ConnectedUserProfile = connect(mapStateToProps)(UserProfile);
+const mapDispatchToProps = {
+    like: (user: User) => likeUser(user)
+};
+
+const ConnectedUserProfile = connect(mapStateToProps, mapDispatchToProps)(UserProfile);
 
 const UserProfileRouted: React.FC = () => {
     let { userId } = useParams();
